525e36bec5cf069c776d9a413a4c1e210b3f8dbb
kaayran <dodororo132@gmail.com>
2022-04-29 10:38:37 +0300

    feat: this class can deploy nodes from NodeConstructor

---
+++ NodeDeployer.cs
@@ -0,0 +1,39 @@
+﻿namespace CustomDataSerialization;
+
+public class NodeDeployer
+{
+    private readonly NodeConstructor _nodeConstructor;
+
+    public NodeDeployer(NodeConstructor nodeConstructor)
+    {
+        _nodeConstructor = nodeConstructor;
+    }
+
+    private int RandomIndex(int lessThen)
+    {
+        var rand = new Random();
+        var randValue = rand.Next(0, lessThen);
+        Console.WriteLine($"RandomIndex: {randValue}");
+
+        return randValue;
+    }
+
+    public void Deploy()
+    {
+        var nodes = _nodeConstructor.Nodes;
+        var rands = _nodeConstructor.Randoms;
+
+        for (var i = 0; i < nodes.Count - 1; i++)
+        {
+            var node = nodes[i];
+            node.Next = nodes[i + 1];
+            node.Prev = nodes[i - 1];
+
+            if (rands <= 0) continue;
+            node.Rand = nodes[RandomIndex(nodes.Count)];
+            rands--;
+        }
+
+        Console.WriteLine("Nodes deployed.");
+    }
+}
