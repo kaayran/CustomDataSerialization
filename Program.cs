e779d8e2643f5e682fa8f081760869b7750bf929
kaayran <dodororo132@gmail.com>
2022-04-29 12:07:12 +0300

    feat: check both from Head->Tail & Tail->Head

--- Program.cs
+++ Program.cs
@@ -5,28 +5,42 @@ public static class Program
     public static void Main(string[] args)
     {
         var cons = new NodeConstructor(10, 3);
-        
+
         var inspector = new NodeInspector(cons);
         inspector.SoftInspect();
 
         var deployer = new NodeDeployer(cons);
         deployer.Deploy();
-        
+
         inspector.HardInspect();
 
         Console.WriteLine($"Head: #{cons.Head.GetHashCode()}, Tail: #{cons.Tail.GetHashCode()}");
-        
-        InspectByNodes(cons.Head);
+
+        InspectByNodesForward(cons.Head);
+        InspectByNodesBackward(cons.Tail);
     }
 
-    private static void InspectByNodes(ListNode head)
+    private static void InspectByNodesForward(ListNode head)
     {
+        Console.WriteLine("Going forward");
         var curr = head;
-        
+
         while (curr != null)
         {
             Console.WriteLine($"{curr.GetHashCode()}");
             curr = curr.Next;
+        }
+    }
+
+    private static void InspectByNodesBackward(ListNode tail)
+    {
+        Console.WriteLine("Going backward");
+        var curr = tail;
+
+        while (curr != null)
+        {
+            Console.WriteLine($"{curr.GetHashCode()}");
+            curr = curr.Prev;
         }
     }
 }
